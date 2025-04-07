9e2744b5e95dd624459b3c27c7bbe5fe17ab7093
huanmin <3426154361@qq.com>
2025-04-07 16:38:57 +0800

    1231

--- null-chain-core/src/main/java/com/gitee/huanminabc/nullchain/NullExt.java
+++ null-chain-core/src/main/java/com/gitee/huanminabc/nullchain/NullExt.java
@@ -6,6 +6,7 @@ import java.io.Externalizable;
 
 /**
  * 使用NULLExt接口,可以在任何对象上直接调用链式操作 ,但是需要保证对象变量不为空
+ * 如果使用了Null.createEmpty 方式创建的对象, 那么返回的一定不是null 而是一个空对象,可以通过空链的方式来处理
  * @param <T>
  */
 public interface NullExt<T> extends NullChainExt<T>{
