e36f667ecc91612596637c8eb9c22efb7794dc90
huanmin <3426154361@qq.com>
2026-01-20 12:14:43 +0800

    chore(util): 添加临时工具类注释说明
    - 为 TransienceUtil 类添加注释说明
    - 标明该工具类为临时实现
    - 注释说明后续将替换为标准工具类

--- null-chain-core/src/main/java/com/gitee/huanminabc/nullchain/common/function/TransienceUtil.java
+++ null-chain-core/src/main/java/com/gitee/huanminabc/nullchain/common/function/TransienceUtil.java
@@ -3,6 +3,9 @@ package com.gitee.huanminabc.nullchain.common.function;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
+/**
+ * 临时工具, 等基础包稳定就替换为标准工具类
+ */
 public class TransienceUtil {
 
     //判断字段是否不能被序列化
